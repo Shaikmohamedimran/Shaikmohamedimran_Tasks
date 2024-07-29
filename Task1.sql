5d94068b3e0996816d7e3fdc8df0705c8cc5fb44
Shaikmohamedimran <shaikmohamedimran43@gmail.com>
2024-07-29 16:40:48 +0530

    Create Task1.sql

---
+++ Task1.sql
@@ -0,0 +1,9 @@
+create table student (
+  ID INT(10),
+  Name VARCHAR2(50),
+  Age TINYINT(50) );
+
+INSERT INTO Student VALUES (1,"ram",21)
+INSERT INTO Student VALUES (2, "shamam", 21)
+
+SELECT * FROM Student;
